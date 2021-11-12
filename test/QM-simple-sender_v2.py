85dd0ae9e9eff8970b93ea1f5ef09d710db5a588
adrifebrer <93275307+adrifebrer@users.noreply.github.com>
2021-11-12 17:19:28 +0100

    Update QM-simple-sender_v2.py

--- test/QM-simple-sender_v2.py
+++ test/QM-simple-sender_v2.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 
     # Create NRF24 object.
     # PLEASE NOTE: PA level is set to MIN, because test sender/receivers are often close to each other, and then MIN works better.
-    nrf = NRF24(pi, ce=25, payload_size=RF24_PAYLOAD.DYNAMIC, channel=100, data_rate=RF24_DATA_RATE.RATE_250KBPS, pa_level=RF24_PA.HIGH)
+    nrf = NRF24(pi, ce=25, payload_size=RF24_PAYLOAD.DYNAMIC, channel=27, data_rate=RF24_DATA_RATE.RATE_250KBPS, pa_level=RF24_PA.HIGH)
     nrf.set_address_bytes(len(address))
     nrf.open_writing_pipe(address)
     
